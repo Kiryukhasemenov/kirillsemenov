d5e34bc075ec3feb80484ae3983cc27fc54852f7
Kiryukhasemenov <kir.semenow@yandex.ru>
2016-12-05 21:25:53 +0300

    Вариант 5, последняя редакция

--- homework7/hw7.py
+++ homework7/hw7.py
@@ -3,12 +3,9 @@ with open("text.txt", "r", encoding="utf-8") as f:
     text=old_text.replace('.', '')
     text=text.replace(',', '')
     words=text.split(' ')
-    print(words)
     number=int()
     for i in range(len(words)):
         if len(words[i])>10:
             number+=1
-            print(words[i])
     percent=(number/len(words))*100
-    print('Всего слов: ',len(words))
     print("Процент слов, содержащих более 10 символов: ", round(percent))
