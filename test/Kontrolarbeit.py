d0b4bc5f9b066dca02fe1218b41e6cae9cf47643
Kiryukhasemenov <kir.semenow@yandex.ru>
2016-12-06 17:33:59 +0400

    Add files via upload

---
+++ test/Kontrolarbeit.py
@@ -0,0 +1,18 @@
+f=open('freq.txt', 'r', encoding='utf-8')
+lines=f.readlines()
+for i in range(len(lines)):
+    word_charact=lines[i].split(' | ')
+    if word_charact[1]=='союз':
+        print(lines[i])
+print("Кажется, первое задание выполнено. Перейдем ко второму:")
+femine_nouns=[]
+ipm_sum=int()
+for i in range(len(lines)):
+    word_charact=lines[i].split(' | ')
+    word_charact2=word_charact[1].split(' ')
+    if word_charact2[0]=="сущ"  and word_charact2[2]=="ед" and word_charact2[3]=="жен":
+        print(word_charact[0])
+        femine_nouns.append(word_charact[0])
+print(femine_nouns)
+        
+## and word_charact2[2]=="ед" and word_charact2[3]=="жен"
