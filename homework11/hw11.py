6f71e07c999bbb3142c34ff535f867061073bb4e
Kiryukhasemenov <kir.semenow@yandex.ru>
2017-01-27 11:17:45 +0300

    вторая редакция домашней работы; добавил словоформы причастий

--- homework11/hw11.py
+++ homework11/hw11.py
@@ -8,7 +8,7 @@ def word_form_finder():
             token=tokens[i]
             token=token.strip('.,!?")')
             bare_tokens.append(token)
-    eat_forms="(С|с)ъе(м|ш|с|д|л|в)(ь|т|и|я|а|о|ш)?(м|т|и)?(е|и)?"
+    eat_forms="(С|с)ъе(м|ш|с|д|л|в)(ь|т|и|я|а|о|ш|е)?(м|т|и|а|е|у|н)?(е|и|м|й|н|г|х|а|о|ы|я|ю)?(ы|и|а|у|о)?(х|ю|г|я|е|м|й)?(и|у|о)?"
     for i in range(len(bare_tokens)):
         result=re.search(eat_forms,bare_tokens[i])
         if result != None:
