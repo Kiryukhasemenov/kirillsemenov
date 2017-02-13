22f33e854f156af089dfa6df4da046bab15d5205
Kiryukhasemenov <kir.semenow@yandex.ru>
2017-02-13 22:41:16 +0300

    Вторая редакция, вариант №5

--- homework13/hw13.py
+++ homework13/hw13.py
@@ -1,9 +1,12 @@
 import re
-def great_changes():
+def change():
     with open ('text.txt', 'r', encoding='utf-8') as source:
         old_text=source.readlines()
-    for line in old_text:
-        newline=re.sub(r'(\s|\(|«)динозавр((а|о|у|е|ы|»)?(м|в|х|\)|»)?и?(\s|.|,|:|;)?\s)', r'\1кот\2', line, flags=re.DOTALL)
-        newline1=re.sub(r'\sДинозавр((а|о|у|е|ы)?(м|в|х)?и?(\s|.|,|:|;)?\s)', r' Кот\1', newline, flags=re.DOTALL)
-        print (newline1)
+    with open ('newtext.txt', 'w', encoding='utf-8') as blocknote:
+        for line in old_text:
+            newline=re.sub(r'(\s|\(|«)?динозавр((а|о|у|е|ы)?(м|в|х)?и?(\s|.|,|:|;)?(\[|\s|\)|»))', r'\1кот\2', line, flags=re.DOTALL)
+            newline1=re.sub(r'(\s|\(|«)?Динозавр((а|о|у|е|ы)?(м|в|х)?и?(\s|.|,|:|;)?(\[|\s|»))', r'\1Кот\2', newline, flags=re.DOTALL)
+            blocknote.write(newline1)
     return 'That`s all!'
+
+change()
