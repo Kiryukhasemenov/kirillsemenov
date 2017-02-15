5a12a8c129966c140cf09e0e0afaf81527cb4f76
Kiryukhasemenov <kir.semenow@yandex.ru>
2017-02-16 01:07:04 +0300

    последняя редакция, единственное добавление - значок ударения в ре.саб

--- homework13/hw13.py
+++ homework13/hw13.py
@@ -5,7 +5,7 @@ def change():
     with open ('newtext.txt', 'w', encoding='utf-8') as blocknote:
         for line in old_text:
             newline=re.sub(r'(\s|\(|«)?динозавр((а|о|у|е|ы)?(м|в|х)?и?(\s|.|,|:|;)?(\[|\s|\)|»))', r'\1кот\2', line, flags=re.DOTALL)
-            newline1=re.sub(r'(\s|\(|«)?Динозавр((а|о|у|е|ы)?(м|в|х)?и?(\s|.|,|:|;)?(\[|\s|»))', r'\1Кот\2', newline, flags=re.DOTALL)
+            newline1=re.sub(r'(\s|\(|«)Диноз́?а́?вр?((а|о|у|е|ы)?(м|в|х)?и?(\s|.|,|:|;)?(\[|\s|»))', r'\1Кот\2', newline, flags=re.DOTALL)
             blocknote.write(newline1)
     return 'That`s all!'
 
