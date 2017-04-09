fff9172a7308f2821a9c577f25ad755fbbf1a62f
Kiryukhasemenov <kir.semenow@yandex.ru>
2017-04-09 22:47:22 +0300

    кажется, готово

--- homework14/hw14.py
+++ homework14/hw14.py
@@ -6,22 +6,60 @@ def s():
     sentences=[]
     sentences=text.split('.')
     nsents=[re.sub(':|,|-|;', '', sentences[i], flags=re.DOTALL) for i in range(len(sentences))]
-##    for i in range(len(sentences)):
-##        sent=sentences[i] 
-##        nsent=re.sub(':|,|-', '', sent, flags=re.DOTALL)
-##        nsents.append(nsent)
-##    sents=[i.strip(',-') for i in sentences]
     return nsents
-def gain(sents):
+
+def word(sents):
     words=[sent.split() for sent in sents]
-##    for l in range(len(words[i])):
-##        if l not in words[i]:
-##            letters.append(l)
-##        else
-    for word in words:
-        letters=list(word)
-        l_freq={}
-##    
-##    for sent in range(len(sents)):
-##        words=sent.split()
     return words
+##def num():
+##def plus(w):
+##    for each in w:
+##        letnums={}
+##        for l in each:
+##            letnums[l]=each.count(l)
+##    ##    return letnums
+##
+##        newword=[]
+##        for letter in each:
+##            n=letnums[letter]
+##            print(letter, ' ', n)
+##            newlet = "{:{let}<{nl}}"
+##            newlet = newlet.format(letter, let=letter, nl=n)        
+##            print (newlet)
+##            newword.append(newlet)
+##        newword=''.join(newword)
+##    return newword
+
+def num(w):
+   # print(w)
+    for sent in w:
+        newsent=[]
+        for wort in sent:
+         #   print(wort)
+            letnums={}
+            
+            newword=[]
+            for let in wort:
+                letnums[let]=wort.count(let)
+
+                n=letnums[let]
+          #     print(let, ' ', n)
+                newlet = "{:{l}<{nl}}"
+                newlet = newlet.format(let,l=let, nl=n)        
+          #      print (newlet)
+                newword.append(newlet)
+            newword=''.join(newword)
+            newsent.append(newword)
+       #     print(newword)
+        newsent=' '.join(newsent)
+        print(newsent)
+
+
+        
+##        newlet = "{:{letnum}<1}"
+##        newlet = newlet.format(letter, letnum=letnums[letter])
+
+##        newlet='{:letnums[l]<1}'
+##        newlet.format(letter)
+    return 'that\`s all!'
+num(word(s()))
